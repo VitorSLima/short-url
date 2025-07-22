13264a35430985ed862dbd771e45b51f49a31a2a
VitorSLima <dev.vitorlima@gmail.com>
2025-07-22 16:21:16 -0400

    feat: integrate Swagger for API documentation and update server port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Short Code API')
+    .setDescription('The Short Code API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addTag('url')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 80);
 }
 bootstrap();
